fce328c6591676fcbbc18393557096c82b1806bc
liuchuanzheng <1052374416@qq.com>
2017-04-07 00:16:31 +0800

    完成欢迎界面和主界面的主框架,添加butterknife

---
+++ app/src/main/java/com/liuchuanzheng/lcz_guigumall/activitys/MainActivity.java
@@ -0,0 +1,29 @@
+package com.liuchuanzheng.lcz_guigumall.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+
+import com.liuchuanzheng.lcz_guigumall.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @Bind(R.id.frameLayout)
+    FrameLayout frameLayout;
+    @Bind(R.id.rg_main)
+    RadioGroup rgMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //将butterKnife与布局文件绑定
+        ButterKnife.bind(this);
+        //默认选中第一个按钮
+        rgMain.check(R.id.rb_home);
+    }
+}
---
+++ app/src/main/java/com/liuchuanzheng/lcz_guigumall/activitys/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.liuchuanzheng.lcz_guigumall.activitys;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.liuchuanzheng.lcz_guigumall.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //两秒后启动主界面
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //这是运行在主线程的,因为handler在主线程创建
+                //开启主界面,并关闭自己
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
